1e05bb0ea33e930bcd90688e4e7f18a35ee7bad3
Robyn Moncrief <44477525+12obyn@users.noreply.github.com>
2019-02-21 12:44:21 -0600

    Update mod 3 mpg.py
    Read comments in code

--- mod 3 mpg.py
+++ mod 3 mpg.py
@@ -1,8 +1,12 @@
+# Missing authorship comments
+# Also, watch out for code plagiarism - read this article https://www.turnitin.com/blog/plagiarism-and-programming-how-to-code-without-plagiarizing-2
+# Especially if this is something that you use as your own sample code later.
+
 # Calculate Miles Per Gallon
 print("This program calculates mpg.")
  
 # Get miles driven from the user
-miles_driven = input("Enter miles driven:")
+miles_driven = input("Enter miles driven:") # Could simplify the next couple lines by wrtiting miles_driven = float(input("Enter miles driven:"))
 # Convert text entered to a
 # floating point number
 miles_driven = float(miles_driven)
